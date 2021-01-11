4fa719d153c2de23c4c426e1a3e2f26115928506
Orkhan Talibli <orkhan.talibli@gmail.com>
2021-01-11 20:22:04 +0400

    to-3 FirstCommit

--- src/views/aston-martin-mini/aston-martin-mini.tsx
+++ src/views/aston-martin-mini/aston-martin-mini.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import React from "react";
 import { useStyles } from "./aston-martini-mini.style";
 import InstagramIcon from '@material-ui/icons/Instagram';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
   //import { t } from "i18n";
 
 
@@ -18,44 +19,42 @@ export const AstonMartinMini = () => {
           {/* ---------------HEADER------------- */}
           <Grid item container className={classes.firstSubContainer}>
             <Grid item xs={4} className={classes.menuIcon}>
-              <img src={require('../../assets/aston-martini/Menu.png')} alt="fireSpot" />
+              <img src={require('../../assets/aston-martini/Menu.png')} alt="fireSpot" className={classes.iconMenu} />
             </Grid>
             <Grid item xs={4} className={classes.iconAston}>
-              <div>
-                <img src={require('../../assets/aston-martini/logo.png')} alt="fireSpot" />
+              <div >
+                <img src={require('../../assets/aston-martini/logo.png')} alt="fireSpot" className={classes.logoIcon}/>
               </div>
             </Grid>
             <Grid item container xs={4} className={classes.socialMediaIcon} >
               <Grid item>
                 <a href="">
-                  <img src={require('../../assets/aston-martini/facebookLogo.png')} alt="fireSpot" />
+                  <img src={require('../../assets/aston-martini/facebookLogo.png')} alt="fireSpot"className={classes.iconMenu} />
                 </a>
               </Grid>
               <Grid item className={classes.twitterIcon}>
                 <a href="">
-                  <img src={require('../../assets/aston-martini/twitterLogo.png')} alt="fireSpot" />
+                  <img src={require('../../assets/aston-martini/twitterLogo.png')} alt="fireSpot" className={classes.iconMenu} />
                 </a>
               </Grid>
               <Grid item>
                 <a href="">
                   <InstagramIcon className={classes.instagram} />
-                  {/* <img src={require('../../assets/aston-martini/instagramLogo (2).png')} alt="fireSpot" /> */}
-                  {/* <img src={require('../../assets/aston-martini/instagramLogo.png')} alt="fireSpot" className={classes.instagramIcon}  /> */}
                 </a>
               </Grid>
 
             </Grid>
           </Grid>
 
-          <Grid item md={7} lg={7}  >        {/*/////className={classes.firstSide}> */}
+          <Grid item md={7} lg={7}  >        
           </Grid>
 
           <Grid item md={5} lg={5} className={classes.secondSide}>
             <Grid item xs={12}>
-              <Typography className={classes.firstWord}>ASTON MARTIN</Typography>
+              <Typography color="textSecondary" className={classes.firstWord}>ASTON MARTIN</Typography>
             </Grid>
             <Grid item>
-              <p className={classes.loremIpsum}>LOREM IPSUM <br /><span className={classes.dolorNone}>DOLOR SIT AMET</span></p>
+              <Typography  className={classes.loremIpsum}>LOREM IPSUM <br /><span className={classes.dolorNone}>DOLOR SIT AMET</span></Typography>
             </Grid>
             <Grid item>
               <Typography className={classes.eighthTypo}>Lorem   ipsum   dolor   sit  amet,   consectetur <span className={classes.continueText}>adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna</span></Typography>
@@ -70,20 +69,20 @@ export const AstonMartinMini = () => {
         {/* ----------------------SECOND CONTAINER----------------------------- */}
 
         <Grid container xs={12} className={classes.secondContainer} >
-          <Grid item xs={12} sm={12} md={6} lg={5} className={classes.leftSide}>
+          <Grid item xs={12} sm={12} md={6} lg={5} className={clsx(classes.leftSide,classes.theSameClass)}>
             <div className={classes.freeDiv}></div>
             <div className={classes.firstDiv}>
-              <Typography className={classes.secondTypo}>Aston Martin's first SUV</Typography>
+              <Typography  className={classes.secondTypo}>Aston Martin's first SUV</Typography>
               <Typography className={classes.thirdTypo}>DBX</Typography>
               <a href="" className={classes.secondBtn}>DAHA ÇOX</a>
               {/* <Button >DAHA ÇOX</Button> */}
             </div>
           </Grid>
 
-          <Grid item xs={12} md={6} lg={7} className={classes.rightSide}>
+          <Grid item xs={12} md={6} lg={7}>
             <Grid item xs={12} className={classes.secondDiv}>
               <Typography className={classes.shortInfo}>QISA MƏLUMAT</Typography>
-              <Typography className={classes.longInfo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              <Typography color="textSecondary" className={classes.longInfo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <span className={classes.loremText}> exercitation ullamco laboris nisi ut
                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
@@ -96,12 +95,12 @@ export const AstonMartinMini = () => {
               <a href="" className={classes.textBtn}>DAHA ÇOX</a>
             </Grid>
             <Grid item xs={12} className={classes.fifthDiv}>
-              <Grid item xs={12} lg={6} className={classes.thirdDiv}>
+              <Grid item xs={12} lg={6} className={clsx(classes.thirdDiv,classes.theSameClass3)}>
                 <Typography className={classes.interyer}>INTERYER</Typography>
                 <Typography className={classes.textLorem}>Lorem ipsum dolor amet, consectetur adipiscing elit,</Typography>
                 <a href="" className={classes.thirdBtn}>DAHA ÇOX</a>
               </Grid>
-              <Grid item xs={12} lg={6} className={classes.fourthDiv}>
+              <Grid item xs={12} lg={6} className={clsx(classes.fourthDiv,classes.theSameClass3)}>
                 <Typography className={classes.interyer}>EKSTERYER</Typography>
                 <Typography className={classes.textLorem}>Lorem ipsum dolor amet, consectetur adipiscing elit,</Typography>
                 <a href="" className={classes.thirdBtn}>DAHA ÇOX</a>
@@ -118,16 +117,16 @@ export const AstonMartinMini = () => {
         <Grid container className={clsx(classes.section, classes.thirdContainer)}>
           <a href="">
             <div className={classes.sixthDiv}>
-
-              <img src={require('../../assets/aston-martini/circleIcon.png')} alt="fireSpot" />
-              <Typography className={classes.playWord}>PLAY</Typography>
+            < PlayCircleOutlineIcon className={classes.playWord}/>
+              {/* <img src={require('../../assets/aston-martini/circleIcon.png')} alt="fireSpot" />
+              <Typography className={classes.playWord}>PLAY</Typography> */}
             </div>
           </a>
         </Grid>
         {/* ----------------------FOURTH CONTAINER----------------------------- */}
 
         <Grid container xs={12} className={classes.secondContainer} >
-          <Grid item xs={12} sm={12} md={6} lg={5} className={classes.leftSide1}>
+          <Grid item xs={12} sm={12} md={6} lg={5} className={clsx(classes.leftSide1,classes.theSameClass)}>
             <div className={classes.freeDiv}></div>
             <div className={classes.firstDiv}>
               <Typography className={classes.secondTypo}>The Archetypal Hunter</Typography>
@@ -136,10 +135,10 @@ export const AstonMartinMini = () => {
               {/* <Button >DAHA ÇOX</Button> */}
             </div>
           </Grid>
-          <Grid item xs={12} md={6} lg={7} className={classes.rightSide}>
+          <Grid item xs={12} md={6} lg={7} >
             <Grid item xs={12} className={classes.secondDiv}>
               <Typography className={classes.shortInfo}>QISA MƏLUMAT</Typography>
-              <Typography className={classes.longInfo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              <Typography color="textSecondary" className={classes.longInfo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <span className={classes.loremText}>exercitation ullamco laboris nisi ut
                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
@@ -152,12 +151,12 @@ export const AstonMartinMini = () => {
               <a href="" className={classes.textBtn}>DAHA ÇOX</a>
             </Grid>
             <Grid item xs={12} className={classes.fifthDiv}>
-              <Grid item xs={12} lg={6} className={classes.thirdDiv1}>
+              <Grid item xs={12} lg={6} className={clsx(classes.thirdDiv1,classes.theSameClass3)}>
                 <Typography className={classes.interyer}>INTERYER</Typography>
                 <Typography className={classes.textLorem}>Lorem ipsum dolor amet, consectetur adipiscing elit,</Typography>
                 <a href="" className={classes.thirdBtn}>DAHA ÇOX</a>
               </Grid>
-              <Grid item xs={12} lg={6} className={classes.fourthDiv1}>
+              <Grid item xs={12} lg={6} className={clsx(classes.fourthDiv1,classes.theSameClass3)}>
                 <Typography className={classes.interyer}>EKSTERYER</Typography>
                 <Typography className={classes.textLorem}>Lorem ipsum dolor amet, consectetur adipiscing elit,</Typography>
                 <a href="" className={classes.thirdBtn}>DAHA ÇOX</a>
@@ -167,12 +166,12 @@ export const AstonMartinMini = () => {
         </Grid>
         {/* ----------------------FIFTH CONTAINER----------------------------- */}
 
-        <Grid container className={clsx(classes.section, classes.fifthContainer)}>
+        <Grid container className={clsx(classes.section, classes.fifthContainer,classes.theSameClasses1)}>
           <a href="">
             <div className={classes.sixthDiv}>
-
-              <img src={require('../../assets/aston-martini/circleIcon.png')} alt="fireSpot" />
-              <Typography className={classes.playWord}>PLAY</Typography>
+               < PlayCircleOutlineIcon className={classes.playWord}/>
+              {/* <img src={require('../../assets/aston-martini/circleIcon.png')} alt="fireSpot" />
+              <Typography className={classes.playWord}>PLAY</Typography> */}
             </div>
           </a>
         </Grid>
@@ -180,11 +179,11 @@ export const AstonMartinMini = () => {
 
         <Grid container xs={12} className={clsx(classes.section, classes.sixthContainer)} >
           {/* <div className={classes.seventhDiv}></div> */}
-          <Grid item sm={8} lg={5} className={classes.eighthDiv}>
+          <Grid item sm={12} lg={5} className={classes.eighthDiv}>
             <div className={classes.eleventhDiv}></div>
             <Typography className={classes.sixthTypo}>ƏLAQƏ</Typography>
             <Typography className={classes.seventhTypo}>Lorem ipsum dolor sit amet, consectetur <br />adipiscing elit,sed <span className={classes.loremText}> do eiusmod tempor</span></Typography>
-            <Grid item container xs={12} className={classes.ninthDiv}>
+            <Grid item container xs={12} sm={12}  className={classes.ninthDiv}>
               <TextField label="AD" variant="outlined" className={classes.firstInput} />
               <TextField label="SOYAD" variant="outlined" className={classes.firstInput} />
               <TextField label="EMAİL" variant="outlined" className={classes.firstInput} />
